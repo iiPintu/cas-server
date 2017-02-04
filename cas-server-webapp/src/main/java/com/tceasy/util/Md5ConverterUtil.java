a28331ffe94ae338aeed4c46b79a183d1e783e4b
liyunhe <yunhe.li@tingjiandan.com>
2017-02-04 19:17:55 +0800

    init

---
+++ cas-server-webapp/src/main/java/com/tceasy/util/Md5ConverterUtil.java
@@ -0,0 +1,42 @@
+package com.tceasy.util;
+
+import org.apache.commons.codec.digest.DigestUtils;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Md5ConverterUtil {
+	
+	public static String Md5(String plainText) {
+		String afterStr = null;
+		try {
+			MessageDigest md = MessageDigest.getInstance("MD5");
+			md.update(plainText.getBytes());
+			byte b[] = md.digest();
+			int i;
+			StringBuffer buf = new StringBuffer("");
+			for (int offset = 0; offset < b.length; offset++) {
+				i = b[offset];
+				if (i < 0)
+					i += 256;
+				if (i < 16)
+					buf.append("0");
+				buf.append(Integer.toHexString(i));
+			}
+			afterStr = buf.toString();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return afterStr;
+	}
+	
+   public static String MD5Encode(String origin,String charset){
+        try {
+            return DigestUtils.md5Hex(origin.getBytes(charset));
+        } catch (UnsupportedEncodingException e) {
+            return null;
+        }
+    }
+
+}
